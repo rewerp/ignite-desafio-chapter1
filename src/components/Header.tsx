b2acf1d65ce228ee17324a5bf32cf118a2bcc3e1
rewerp <rewer_p@hotmail.com>
2021-11-23 00:03:37 -0300

    23-11-2021-00-02
    Finalizado desafio
    implementado as funções nos arquivos
    - Header.tsx
    - TaskLits.tsx
    - TodoInput.tsx
    - Home.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,7 +9,7 @@ interface HeaderProps {
 }
 
 export function Header({ tasksCounter }: HeaderProps) {
-  const tasksCounterText = TODO render 'tarefa' if tasksCounter equals 1, otherwise render 'tarefas'
+  const tasksCounterText = tasksCounter === 1 ? `tarefas` : `tarefas`;
   
   return (
     <View style={styles.container}>
