55615d12fa0ac69a470bd25005991d86767c023e
rewerp <rewer_p@hotmail.com>
2021-11-25 18:01:01 -0300

    commit-25-11-2021-18-00
    - Desafio complementar

--- src/components/TaskItem.tsx
+++ src/components/TaskItem.tsx
@@ -1,11 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FlatList, Image, TouchableOpacity, View, Text, StyleSheet, FlatListProps } from 'react-native';
 import Icon from 'react-native-vector-icons/Feather';
 
 import { ItemWrapper } from './ItemWrapper';
 
 import trashIcon from '../assets/icons/trash/trash.png'
-import { TasksItem } from './TaskItem';
 
 export interface Task {
   id: number;
@@ -13,31 +12,55 @@ export interface Task {
   done: boolean;
 }
 
-interface TasksListProps {
+interface TasksItemProps {
   tasks: Task[];
   toggleTaskDone: (id: number) => void;
   removeTask: (id: number) => void;
   editTask: (id: number) => void;
 }
 
-export function TasksList({ tasks, toggleTaskDone, removeTask, editTask }: TasksListProps) {
+const [taskEditing, setTaskediting] = useState(false);
+const [taskNewTitle, setTasNewTitle] = useState(task.title);
+
+export function TasksItem({ tasks, toggleTaskDone, removeTask, editTask }: TasksItemProps) {
   return (
-    <FlatList
-      data={tasks}
-      keyExtractor={item => String(item.id)}
-      contentContainerStyle={{ paddingBottom: 24 }}
-      showsVerticalScrollIndicator={false}
-      renderItem={({ item, index }) => {
-        return (
-          <ItemWrapper index={index}>
-            <TasksItem />
-          </ItemWrapper>
-        )
-      }}
-      style={{
-        marginTop: 32
-      }}
-    />
+    <>
+      <View>
+        <TouchableOpacity
+          testID={`button-${index}`}
+          activeOpacity={0.7}
+          style={styles.taskButton}
+          onPress={() => toggleTaskDone(item.id)}
+        >
+          <View
+            testID={`marker-${index}`}
+            style={item.done ? styles.taskMarkerDone : styles.taskMarker}
+          >
+            {item.done && (
+              <Icon
+                name="check"
+                size={12}
+                color="#FFF"
+              />
+            )}
+          </View>
+
+          <Text
+            style={item.done ? styles.taskTextDone : styles.taskText}
+          >
+            {item.title}
+          </Text>
+        </TouchableOpacity>
+      </View>
+
+      <TouchableOpacity
+        testID={`trash-${index}`}
+        style={{ paddingHorizontal: 24 }}
+        onPress={() => removeTask(item.id)}
+      >
+        <Image source={trashIcon} />
+      </TouchableOpacity>
+    </>
   )
 }
 
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { StyleSheet, View } from 'react-native';
+import { Alert, StyleSheet, View } from 'react-native';
 
 import { Header } from '../components/Header';
 import { Task, TasksList } from '../components/TasksList';
@@ -9,6 +9,7 @@ interface Task {
   id: number;
   title: string;
   done: boolean;
+  editTask: boolean;
 }
 
 export function Home() {
@@ -17,8 +18,23 @@ export function Home() {
   function handleAddTask(newTaskTitle: string) {
     const newTask = {
       id: new Date().getTime(),
-      title :newTaskTitle,
+      title: newTaskTitle,
       done: false
+    }
+    const foundItem = tasks.find(item => item.title === newTaskTitle);
+
+    if (foundItem) {
+      return (
+        Alert.alert(
+          "Task já cadastrada",
+          "Você não pode cadastrar uma task com o mesmo nome",
+          [
+            {
+              text: "OK"
+            }
+          ]
+        )
+      );
     }
 
     setTasks(oldTasks => [...oldTasks, newTask]);
@@ -39,6 +55,30 @@ export function Home() {
   function handleRemoveTask(id: number) {
     const updatedTasks = tasks.filter(task => task.id !== id)
 
+    Alert.alert(
+      "Remover item",
+      "Tem certeza que você deseja remover esse item?",
+      [
+        {
+          text: "Não"
+        },
+        {
+          text: "Sim",
+          onPress: () => setTasks(updatedTasks)
+        }
+      ]
+    )
+  }
+
+  function handleEditTask(taskId: number, taskNewTitle: string) {
+    const updatedTasks = tasks.map(tasks => ({ ...tasks }))
+    const foundItem = updatedTasks.find(item => item.id === taskId);
+
+    if (!foundItem)
+      return;
+
+    foundItem.title = taskNewTitle;
+
     setTasks(updatedTasks);
   }
 
@@ -48,10 +88,10 @@ export function Home() {
 
       <TodoInput addTask={handleAddTask} />
 
-      <TasksList 
-        tasks={tasks} 
+      <TasksList
+        tasks={tasks}
         toggleTaskDone={handleToggleTaskDone}
-        removeTask={handleRemoveTask} 
+        removeTask={handleRemoveTask}
       />
     </View>
   )
